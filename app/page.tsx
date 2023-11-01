4d38dbc592155f151baf684b8a315a2f7cdb3a86
chetandivekar <chetandivekar6@gmail.com>
2023-11-02 00:47:39 +0530

    chetan-gallery-lightbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+"use client";
 import GalleryLightbox from "@/components/Gallery";
 import React from "react";
+import { Gallery } from "gallery-lightbox";
 
 type Props = {};
 
 const Home = (props: Props) => {
   return (
     <>
-      <GalleryLightbox />
+      <Gallery />
+      {/* <GalleryLightbox /> */}
     </>
   );
 };
